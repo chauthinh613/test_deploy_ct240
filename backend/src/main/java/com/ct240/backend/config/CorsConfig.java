1abc53a31c0adf2052701083fa8c1931e6e460b3
aditya <chauthinh613@gmail.com>
2026-03-25 18:36:51 +0700

    hi

--- backend/src/main/java/com/ct240/backend/config/CorsConfig.java
+++ backend/src/main/java/com/ct240/backend/config/CorsConfig.java
@@ -15,7 +15,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOriginPatterns("http://localhost:5173")
+                        .allowedOriginPatterns("http://localhost:5173", "https://frontend-production-64e1.up.railway.app")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
